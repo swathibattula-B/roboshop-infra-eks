64ca5b5f1562b35b9013f64c2d0f817468baeb23
swathi <battulaswathi15407@gmail.com>
2026-06-09 13:38:44 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,6 +1,6 @@
 module "sg" {
     count = length(var.sg_names)
-    source = "../../terraform-aws-sg"
+    source = "../../terraform-aws-sg1"
     project = var.project
     environment = var.environment
     sg_name = replace(var.sg_names[count.index], "_", "-")
---
+++ 31-cicd-tools/variables.tf
@@ -0,0 +1,27 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z077664125UVZ9NFX7BZC"
+}
+
+variable "domain_name" {
+    default = "daws88ss.online"
+}
+
+variable "sonar" {
+    default = true
+}
+
+variable "jenkins" {
+    default = true
+}
+
+variable "runner" {
+    default = false
+}
